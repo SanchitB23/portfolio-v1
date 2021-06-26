6cf2e6ec02af43b1d62dbb0a55da5b1b845053bf
Sanchit Bhatnagar <sanbht23@gmail.com>
2021-06-27 01:33:22 +0530

    added nav onMount animation

--- src/components/layouts/Navbar/index.tsx
+++ src/components/layouts/Navbar/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from "gatsby";
 import IconLogo from "../../icons/Logo";
 import {navLinks} from "../../../config/constants";
 import {LayoutProps} from "../Interface";
-
-const renderNavLinks = () => {
-    return navLinks.map((link, index) => {
-        return <Link to={`#${link}`}
-                     key={index} className="nav__links__item">{link}</Link>
-    })
-}
+import {loaderDelay} from "../../../config/utils";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const NavBar: React.FC<LayoutProps> = ({isHome}) => {
+    const [isMounted, setIsMounted] = useState(false)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setIsMounted(true), 100)
+        return () => clearTimeout(timeout)
+    }, [])
+
+    const timeout = isHome ? loaderDelay : 0;
+    const fadeClass = isHome ? 'fade' : '';
+    const fadeDownClass = isHome ? 'fadedown' : '';
+
     const Logo = (
         <div tabIndex={-1}>
             {isHome ? (
@@ -25,18 +31,55 @@ const NavBar: React.FC<LayoutProps> = ({isHome}) => {
             )}
         </div>
     );
+
+    const ResumeLink = (
+        <a className="btn" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+            Resume
+        </a>
+    );
+
     return (
         <nav className="nav">
-            {Logo}
+            <TransitionGroup component={null}>
+                {isMounted ? (
+                    <CSSTransition classNames={fadeClass} timeout={timeout}>
+                        <>{Logo}
+                        </>
+                    </CSSTransition>
+                ) : null}
+            </TransitionGroup>
             <div className="nav__links">
                 <ol>
-                    {renderNavLinks()}
+                    <TransitionGroup component={null}>
+                        {isMounted && navLinks && navLinks.map((link, index) => (
+                            <CSSTransition key={index} classNames={fadeDownClass} timeout={timeout}>
+                                <li className="nav__links__item" key={index}
+                                    style={{transitionDelay: `${isHome ? index * 100 : 0}ms`}}>
+                                    <Link
+                                        to={`#${link}`}
+
+                                    >{link}</Link>
+                                </li>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
                 </ol>
-                <a className="btn">Resume</a>
+                <TransitionGroup component={null}>
+                    {isMounted && (
+                        <CSSTransition classNames={fadeDownClass} timeout={timeout}>
+                            <div style={{transitionDelay: `${isHome ? navLinks.length * 100 : 0}ms`}}>
+                                {ResumeLink}
+                            </div>
+                        </CSSTransition>
+
+                    )}
+                </TransitionGroup>
             </div>
         </nav>
     );
 };
+
+
 /*const NavBarStyle = styled.nav`
   display: flex;
   align-items: center;
