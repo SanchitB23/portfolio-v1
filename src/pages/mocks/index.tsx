1c9663d75752f50a285647c65d9787b3b8e48eff
Sanchit Bhatnagar <sanbht23@gmail.com>
2022-09-18 16:50:55 +0530

    PR Merge Fixes

--- src/pages/mocks/index.tsx
+++ src/pages/mocks/index.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
-import {graphql, useStaticQuery} from "gatsby";
 
 
 const Test: React.FC = () => {
-    const data = useStaticQuery(query)
-    console.log("data", data)
+    // const data = useStaticQuery(query)
     return (
-        // <Loader finishLoading={(v) => console.log("finish loading", v)}/>
-        <a href={data.allContentfulAboutMe.nodes[0].resume.file.url}>Click Me</a>
-
+        /*
+         * <Loader finishLoading={(v) => console.log("finish loading", v)}/>
+         * <a href={data.allContentfulAboutMe.nodes[0].resume.file.url}>Click Me</a>
+         */
+        <div>Test Component</div>
     )
 };
 
-export const query = graphql`
-    {
-        allContentfulAboutMe {
-            nodes {
-                resume {
-                    file {
-                        url
-                    }
-                }
-            }
-        }
-    }
-`
+/*
+ *export const query = graphql`
+ *  {
+ *      allContentfulAboutMe {
+ *          nodes {
+ *              resume {
+ *                  file {
+ *                      url
+ *                  }
+ *              }
+ *          }
+ *      }
+ *  }
+ *`
+ */
 export default Test;
